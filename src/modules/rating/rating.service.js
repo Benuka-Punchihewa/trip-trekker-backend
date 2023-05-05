07bbce419d5439899a64e91fe8720d2075c5bd52
Benuka Punchihewa <benuka1999@gmail.com>
2023-05-05 21:08:16 +0530

    added paginated ratings

--- src/modules/rating/rating.service.js
+++ src/modules/rating/rating.service.js
@@ -1,3 +1,4 @@
+import constants from "../../constants.js";
 import Rating from "./rating.model.js";
 
 const save = async (rating, session) => {
@@ -14,4 +15,70 @@ const findByIdAndDelete = async (_id, session) => {
   return Rating.findByIdAndDelete({ _id });
 };
 
-export default { save, findById, findByIdAndDelete };
+/*
+ data interfaces
+ 
+ hotel ratings
+ {
+    type: string,
+    hotelId: ObjectId
+ }
+
+ attraction ratings
+ {
+    type: string,
+    attractionId: ObjectId
+ }
+
+ tour guide ratings
+ {
+    type: string,
+    tourGuideId: ObjectId
+ }
+*/
+const findPaginatedRatings = async (data, pageableObj) => {
+  const pipeline = [];
+
+  const queryObj = {
+    type: data.type,
+  };
+
+  if (data.type === constants.RATINGS.RATEES.HOTEL)
+    queryObj["hotel._id"] = data.hotelId;
+  else if (data.type === constants.RATINGS.RATEES.ATTRACTION)
+    queryObj["hotel._id"] = data.attractionId;
+  else queryObj["user._id"] = data.tourGuideId;
+
+  pipeline.push({
+    $match: queryObj,
+  });
+
+  pipeline.push({
+    $sort: {
+      _id: pageableObj.orderBy === "asc" ? 1 : -1,
+    },
+  });
+
+  pipeline.push({
+    $facet: {
+      metadata: [{ $count: "totalElements" }],
+      data: [
+        { $skip: (pageableObj.page - 1) * pageableObj.limit },
+        { $limit: pageableObj.limit },
+      ],
+    },
+  });
+
+  const result = await Rating.aggregate(pipeline);
+
+  const content = result[0].data;
+  const totalElements = result[0]?.metadata[0]?.totalElements || 0;
+
+  return {
+    content,
+    totalElements,
+    totalPages: Math.ceil(totalElements / pageableObj.limit),
+  };
+};
+
+export default { save, findById, findByIdAndDelete, findPaginatedRatings };
