ceda541ca249bad41b9ce399fde2fb77de728710
Sumedha Lakmal Bandaranayaka <74480291+sumedha-95@users.noreply.github.com>
2023-05-05 21:14:27 +0530

    added hotel func

---
+++ src/modules/hotel/hotel.service.js
@@ -0,0 +1,55 @@
+import Hotel from "./hotel.model";
+
+const save = async (hotel, session) => {
+  return hotel.save({ session });
+};
+
+const findById = async (id, session) => {
+  if (session) return Hotel.findById(id).session(session);
+  return Hotel.findById(id);
+};
+
+const findPaginatedHotels = async (
+  keyword = "",
+  pageableObj
+) => {
+  const pipeline = [];
+
+  if (!keyword) keyword = "";
+  const queryObj = {
+    name: { $regex: keyword, $options: "i" },
+  };
+
+  pipeline.push({
+    $match: queryObj,
+  });
+
+  pipeline.push({
+    $sort: {
+      _id: pageableObj.orderBy === "asc" ? 1 : -1,
+    },
+  });
+
+  pipeline.push({
+    $facet: {
+      metadata: [{ $count: "totalElements" }],
+      data: [
+        { $skip: (pageableObj.page - 1) * pageableObj.limit },
+        { $limit: pageableObj.limit },
+      ],
+    },
+  });
+
+  const result = await Hotel.aggregate(pipeline);
+
+  const content = result[0].data;
+  const totalElements = result[0]?.metadata[0]?.totalElements || 0;
+
+  return {
+    content,
+    totalElements,
+    totalPages: Math.ceil(totalElements / pageableObj.limit),
+  };
+};
+
+export default { save, findById, findPaginatedHotels };
